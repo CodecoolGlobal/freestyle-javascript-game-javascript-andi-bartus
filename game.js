f0a68ec2a07de1787e2ef8a3496c631abf02cb8e
bzoli23 <bzoli0523@gmail.com>
2021-06-15 09:52:23 +0200

    m

--- game.js
+++ game.js
@@ -3,5 +3,15 @@ initGame();
 function initGame() {
 
     // Your game can start here, but define separate functions, don't write everything in here :)
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const player1_name = urlParams.get('player1');
+    const player2_name = urlParams.get('player2');
+
+    let player_1 = document.querySelector(".player-1 > p");
+    player_1.innerHTML = player1_name
+
+    let player_2 = document.querySelector(".player-2 > p");
+    player_2.innerHTML = player2_name
 
 }
