46664bf9d9fa574114db46bbf0658224aeb13458
safarandras <safarandras00@gmail.com>
2021-06-16 15:23:36 +0200

    refactor

--- game.js
+++ game.js
@@ -1,97 +1,14 @@
-initGame();
-
-
-
-function initGame() {
-
-    // Your game can start here, but define separate functions, don't write everything in here :)
+function initNames() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const player1_name = urlParams.get('player1');
     const player2_name = urlParams.get('player2');
-
-    let player_1 = document.querySelector(".player-1-cards > p");
-    player_1.innerHTML = player1_name
-
-    let player_2 = document.querySelector(".player-2-cards > p");
-    player_2.innerHTML = player2_name
-    initChips()
-    let suits = ["S", "H", "D", "C"];
-    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-    let deck = [];
-
-    function createDeck() {
-        deck = [];
-        for (let i = 0 ; i < values.length; i++)
-        {
-            for(let x = 0; x < suits.length; x++)
-            {
-                let weight = parseInt(values[i]);
-                if (values[i] === "J" || values[i] === "Q" || values[i] === "K")
-                    weight = 10;
-                if (values[i] === "A")
-                    weight = 11;
-                let card = { Value: values[i], Suit: suits[x], Weight: weight };
-                deck.push(card);
-            }
-        }
-    }
-    createDeck()
-    let hand = '#p1_hand'
-    let player_score = '#p1_score > p'
-    let count = 0
-    let score1 = 0
-    let score2 = 0
-
-
-    function hit_player () {
-
-        let hit_button = document.querySelector(".hit");
-        hit_button.addEventListener('click', function test() {
-            let random_card = deck[Math.floor(Math.random() * (deck.length - 1))]
-            let hand_img = document.createElement('img')
-            hand_img.src = `./cards/${random_card.Value}${random_card.Suit}.png`
-            hand_img.classList.add("card")
-            document.querySelector(hand).appendChild(hand_img)
-
-            let score = document.querySelector(player_score)
-            if (player_score === '#p2_score > p'){
-                score2 += random_card.Weight
-            }
-            else if (player_score === '#p1_score > p'){
-                score1 += random_card.Weight
-            }
-            console.log(score1)
-            console.log(score2)
-            count += random_card.Weight
-            score.innerHTML = "Score: " + count
-        })
-    }
-    function stay() {
-        let stay = document.querySelector(".stay");
-        stay.addEventListener('click', function () {
-            document.querySelector(".player-1-cards").classList.remove("active")
-            hand = '#p2_hand'
-            player_score = '#p2_score > p'
-            count = 0
-            document.querySelector(".player-2-cards").classList.add("active")
-        })
-    }
-    function win(){
-        if (score1 === 21){
-            alert(player1_name + "won the game!")
-        }
-        else if (score2 === 21){
-            alert(player2_name + "won the game!")
-        }
-    }
-    stay()
-    hit_player()
-    win()
+    document.querySelector(".player-1-cards > p").innerHTML = player1_name;
+    document.querySelector(".player-2-cards > p").innerHTML = player2_name;
+    console.log(`${player1_name}, ${player2_name}`)
 }
 
-
-function initChips(){
+function initChips() {
     const bet_field = document.querySelector('.bet-field')
     const chips = document.querySelectorAll('.chips')
     let dragged_item = null
@@ -101,13 +18,8 @@ function initChips(){
         item.draggable = true
         item.addEventListener('dragstart', function (e) {
             dragged_item = e.target
-            let copy = dragged_item.cloneNode(true)
-            dragged_copy = copy
-
-
+            let dragged_copy = dragged_item.cloneNode(true)
         });
-
-
         item.addEventListener('dragend', function () {
             setTimeout(function () {
                 dragged_item.style.display = 'block';
@@ -115,25 +27,114 @@ function initChips(){
             }, 0)
         });
     }
-
-        bet_field.addEventListener('dragover', function (e){
-            e.preventDefault()
-            });
-
-        bet_field.addEventListener('dragenter', function (e){
-            e.preventDefault()
-
-            });
-
-
-        bet_field.addEventListener('drop', function(e){
-
-            let value = Number(dragged_item.dataset.value)
-            bet_counter += value
-            let bet = document.querySelector(".table > p")
-            bet.innerHTML = bet_counter
-            this.appendChild(dragged_copy)
-
-        })
+    bet_field.addEventListener('dragover', function (e) {
+        e.preventDefault()
+    });
+    bet_field.addEventListener('dragenter', function (e) {
+        e.preventDefault()
+    });
+    bet_field.addEventListener('drop', function (e) {
+        let value = Number(dragged_item.dataset.value)
+        bet_counter += value
+        let bet = document.querySelector(".table > p")
+        bet.innerHTML = bet_counter
+        this.appendChild(dragged_copy)
+    })
 }
 
+function createDeck() {
+    let suits = ["S", "H", "D", "C"];
+    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+    let deck = [];
+    for (let value of values) {
+        for (let suit of suits) {
+            let weight = parseInt(value);
+            if (value === "J" || value === "Q" || value === "K")
+                weight = 10;
+            if (value === "A")
+                weight = 11;
+            let card = {Value: value, Suit: suit, Weight: weight};
+            deck.push(card);
+        }
+    }
+    return deck
+}
+
+function playerRound(player, deck) {
+    document.querySelector(".deck").addEventListener('click', function () {
+        getNewCard(player, deck)
+    })
+}
+
+function getNewCard(player, deck) {
+    let randomCard = deck[Math.floor(Math.random() * (deck.length - 1))];
+    let newCard = document.createElement('img');
+    newCard.src = `./cards/${randomCard.Value}${randomCard.Suit}.png`;
+    newCard.classList.add("card");
+    player.Hand.appendChild(newCard);
+    let newScore = parseInt(player.Score.innerHTML) + randomCard.Weight;
+    player.Score.innerHTML = `Score: ${newScore}`;
+}
+
+function dealerRound(deck) {
+    alert("dealer's round!")
+}
+
+function win() {
+    if (score1 === 21) {
+        alert(player1_name + "won the game!")
+    } else if (score2 === 21) {
+        alert(player2_name + "won the game!")
+    }
+}
+
+function initStay(player2, deck) {
+    document.querySelector(".stay").addEventListener('click', function (e) {
+        let active = document.querySelector(".active");
+        if (active.classList.contains("player-1-cards")) {
+            document.querySelector(".deck").removeEventListener("click", getNewCard);
+            active.classList.remove("active");
+            document.querySelector(".player-2-cards").classList.add("active");
+            playerRound(player2, deck)
+        } else if (active.classList.contains("player-2-cards")) {
+            active.classList.remove("active");
+            dealerRound(deck)
+        }
+    })
+}
+
+function initStart(player1, deck) {
+    document.querySelector(".start").addEventListener("click", function (event) {
+        playerRound(player1, deck)
+    })
+}
+
+function initPlayers() {
+    let names = [document.querySelector(".player-1-cards > p").innerHTML,
+        document.querySelector(".player-2-cards > p").innerHTML]
+    let players = []
+    for (let i = 0; i < names.length; i++) {
+        let player = {
+            Name: names[i],
+            Bet: 0,
+            Hand: document.querySelector(`#p${i + 1}_hand`),
+            Score: document.querySelector(`#p${i + 1}_score > p`)
+        }
+        players.push(player)
+        console.log(player)
+    }
+    return players
+}
+let game = {
+    player1: null,
+    player2: null,
+    deck: createDeck()
+    initGame: () => {
+        initNames()
+        [game.player1, game.player2] = initPlayers()
+        initChips()
+        const deck = createDeck()
+        initStart(player1, deck)
+        initStay(player2, deck)
+    }
+}
